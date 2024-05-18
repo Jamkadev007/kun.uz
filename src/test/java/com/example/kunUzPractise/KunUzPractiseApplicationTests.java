aee3d48c5e1ed8a428d6634a2a7ad49ff3d8a71c
Jamkadev007 <jamshidbekbekmetov@gmail.com>
2024-05-18 14:35:05 +0500

    src/ qo'shildi

---
+++ src/test/java/com/example/kunUzPractise/KunUzPractiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kunUzPractise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KunUzPractiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
